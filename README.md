165a553ad0d544170bfd6c59d857060118b66212
Ivan Fdez <36510312+IvanFdez2001@users.noreply.github.com>
2023-09-08 14:00:46 +0200

    Code reorganized in a cleaner way

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # PortScanner
 
-Python script that performs a scan of open ports on a specific IP address
+Python script that performs a scan of open ports on a specific IP address 
+
+Permissions must be given or the script must be run as sudo for it to work properly
 
 # Necessary dependences
 
--- scan.py
+++ scan.py
@@ -22,21 +22,12 @@ def clear_terminal():
 
 
 def scan():
-    pass
 
-
-def main():
-
-    clear_terminal()
-
-    print(Fore.LIGHTCYAN_EX + Style.BRIGHT + fig.renderText("Port Scanning") +
-          "\n--------------- Code By @IvanFdez2001 ---------------" + Fore.RESET)
-    print("\n")
-
-    ip = input("Enter the IP address on which you want the scan to be performed: ")
+    ip = input(
+        "\nEnter the IP address on which you want the scan to be performed: ")
 
     nm = nmap.PortScanner()
-    r = nm.scan(hosts=ip, arguments="-p- -sS -sC -sV --min-rate 5000 -vvv -n")
+    nm.scan(hosts=ip, arguments="-p- -sS -sC -sV --min-rate 5000 -vvv -n")
 
     print("\n" + Fore.BLUE + "Host: " +
           Fore.LIGHTRED_EX + ip + Fore.RESET)
@@ -47,16 +38,24 @@ def main():
 
         print("\n----------------------------------------------------------------")
         print(Fore.BLUE + "Protocol: " +
-              Fore.LIGHTRED_EX + protocol + Fore.RESET)
+              Fore.LIGHTRED_EX + protocol + Fore.RESET + "\n")
 
         lport = nm[ip][protocol].keys()
         sorted(lport)
 
         for port in lport:
-            print("\t Port: %s \t State: %s \t Service: %s" %
-                  (port, nm[ip][protocol][port]["state"], nm[ip][protocol][port]["name"]))
+            print("\n\t Port: %s \t State: %s \t Service: %s \t Version: %s\n" %
+                  (port, nm[ip][protocol][port]["state"], nm[ip][protocol][port]["name"], nm[ip][protocol][port]["version"]))
 
-    print()
+
+def main():
+
+    clear_terminal()
+
+    print(Fore.LIGHTCYAN_EX + Style.BRIGHT + fig.renderText("Port Scanning") +
+          "\n--------------- Code By @IvanFdez2001 ---------------" + Fore.RESET)
+
+    scan()
 
 
 main()
